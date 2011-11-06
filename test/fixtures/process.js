a514ac2a4209ae75aed852476009bd18f471e44b
Swizec Teller <swizec@swizec.com>
2011-11-06 23:26:24 +0100

    able to detect process.ENV

--- test/fixtures/process.js
+++ test/fixtures/process.js
@@ -1,7 +1,3 @@
 
 process.ENV;
-process.ARGV;
-process.binding('stdio');
-process.binding('net');
-process.memoryUsage().vsize;
 
--- test/llamaduck.js
+++ test/llamaduck.js
@@ -4,9 +4,19 @@ var path = require('path');
 var util = require('util');
 
 exports['test basic parsing'] =  function (beforeExit, assert) {
-
     llamaduck.get_ast(path.resolve('./test/fixtures/process.js'),
                       function (ast) {
                           assert.ok(ast[1].length >= 5);
+                      });
+};
+
+exports['test process check'] = function (beforeExit, assert) {
+    llamaduck.get_ast(path.resolve('./test/fixtures/process.js'),
+                      function (ast) {
+                          var warnings = llamaduck.check_process(ast);
+
+                          console.log(warnings);
+
+                          assert.eql(warnings.length, 5);
                       });
 };
